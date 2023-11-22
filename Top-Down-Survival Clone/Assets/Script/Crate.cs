b1dec2074f0ae832225f25f6e8c5b9574dda6e18
vickyaquino <vickyaquino1989@gmail.com>
2023-11-21 18:37:42 -0800

    inported low poly assets

--- Top-Down-Survival Clone/Assets/Script/Crate.cs
+++ Top-Down-Survival Clone/Assets/Script/Crate.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Crate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int CoinsContained;
+    public GameObject CoinPrefab;
+    public GameObject crate;
+
+    public void dropCoins()
     {
-        
+        for (int i = 0; i < CoinsContained; i++)
+        {
+            GameObject temp = Instantiate(CoinPrefab);
+            temp.transform.position = GetRandomPointInCollider(GetComponent<BoxCollider>());
+        }
+        crate.GetComponent<MeshRenderer>().enabled=false;
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        this.gameObject.GetComponent<CapsuleCollider>().enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    Vector3 GetRandomPointInCollider(Collider collider)
     {
-        
+        Vector3 point = new Vector3(Random.Range(collider.bounds.min.x, collider.bounds.max.x),
+            Random.Range(collider.bounds.min.y, collider.bounds.max.y), Random.Range(collider.bounds.min.z, collider.bounds.max.z));
+        if (point != collider.ClosestPoint(point))
+        {
+            point = GetRandomPointInCollider(collider);
+        }
+        point.y = 1;
+        return point;
     }
 }
