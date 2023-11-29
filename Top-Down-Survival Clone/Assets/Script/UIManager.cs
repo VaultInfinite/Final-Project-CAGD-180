72c57141afc83d69ecec127e9865ae08b8fe9690
vickyaquino <vickyaquino1989@gmail.com>
2023-11-28 20:04:28 -0800

    Got the UI working, the build settings are applied as well
    :D

--- Top-Down-Survival Clone/Assets/Script/UIManager.cs
+++ Top-Down-Survival Clone/Assets/Script/UIManager.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool IsPaused;
+    public GameObject PausePanel;
+    public Slider HealthSlider;
+    public PlayerController PlayerScript;
+    public TextMeshProUGUI HealthText;
+
+    public void startRestart()
     {
-        
+        SceneManager.LoadScene(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play()
     {
-        
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoseGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void WinGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (this.gameObject.CompareTag("Pause"))
+        {
+            if (Input.GetKeyDown(KeyCode.Escape) && !IsPaused)
+            {
+                PauseGame();
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape) && IsPaused)
+            {
+                ResumeGame();
+            }
+            HealthSlider.maxValue = PlayerScript.healthLimit;
+            HealthSlider.value = PlayerScript.health;
+            HealthText.text = PlayerScript.health.ToString();
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        PausePanel.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        IsPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        PausePanel.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        IsPaused = false;
     }
 }
